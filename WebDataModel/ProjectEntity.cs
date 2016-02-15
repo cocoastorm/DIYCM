75f1253fc0b0e9332b1ae6c6cc2d870a2e36059e
Trevor Broderick <A00925723@ad.bcit.ca>
2016-02-15 12:24:28 -0800

    Added the WebDataModel, including the ProjectEntity model and the MainCategoryEntity model

---
+++ WebDataModel/ProjectEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebDataModel {
+    public class ProjectEntity {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [MaxLength(100)]
+        public string ProjectName { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        public DateTime ProjectedStartDate { get; set; }
+
+        public DateTime ActualStartDate { get; set; }
+
+        public DateTime ProjectedFinishDate { get; set; }
+
+        public DateTime ActualFinishDate { get; set; }
+    }
+}
