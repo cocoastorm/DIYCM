0e78e4d54626e5f96283cabfeed460ccb04a22ca
Ricky Chen <rickyshen1991@hotmail.com>
2016-02-15 21:56:03 +0000

    added subcategory entity and area entity model

---
+++ WebDataModel/AreaEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDataModel
+{
+    public class AreaEntity
+    {           
+        [Key]
+        [MaxLength(100)]
+        public int AreaId { get; set; }
+
+        [MaxLength(50)]
+        public String AreaRoom { get; set; }
+
+        public decimal AreaSquareFottage { get; set; }
+    }
+}
