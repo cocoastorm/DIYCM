2f488e7c67c62ac0c951c8ba3cac629820a8f6b3
Andrew Walsh <andrew.walsh20@gmail.com>
2016-02-15 13:44:52 -0800

    Created SupplierInvoiceDetail and SupplierInvoiceHeader

---
+++ WebDataModel/SupplierInvoiceDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDataModel
+{
+    class SupplierInvoiceDetail
+    {
+        [MaxLength(20)]
+        [ForeignKey("SupplierInvoiceHeader")]
+        public string InvoiceId { get; set; }
+
+        public int LineNumber { get; set; }
+
+        [MaxLength(20)]
+        public string PartNumber { get; set; }
+
+        [MaxLength(255)]
+        public string PartDescription { get; set; }
+
+        [MaxLength(6)]
+        [ForeignKey("SupplierInvoiceHeader")]
+        public int? CategoryId { get; set; }
+
+        [MaxLength(4)]
+        [ForeignKey("SupplierInvoiceHeader")]
+        public int? SubCategoryId { get; set; }
+
+        [MaxLength(4)]
+        [ForeignKey("SupplierInvoiceHeader")]
+        public int? AreaId { get; set; }
+
+        public float UnitPrice { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ WebDataModel/SupplierInvoiceHeader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDataModel
+{
+    class SupplierInvoiceHeader
+    {
+        [MaxLength(20)]
+        [Key]
+        public string InvoiceId { get; set; }
+
+        [MaxLength(50)]
+        public string SupplierName { get; set; }
+
+        [MaxLength(4)]
+        public int QuoteHeaderId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [MaxLength(50)]
+        public string ContactName { get; set; }
+
+        [MaxLength(13)]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(50)]
+        public string ReferredBy { get; set; }
+
+        [MaxLength(100)]
+        public string AddressStreet { get; set; }
+
+        [MaxLength(30)]
+        public string AddressCity { get; set; }
+
+        [MaxLength(30)]
+        public string AddressProvince { get; set; }
+
+        [MaxLength(15)]
+        public string AddressPostalCode { get; set; }
+
+        [MaxLength(15)]
+        public string AddressCountry { get; set; }
+
+        public Boolean AmountPaid { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        public decimal SH_AMOUNT { get; set; }
+
+        public decimal SH_AMOUNT_PAID { get; set; }
+    }
+}
