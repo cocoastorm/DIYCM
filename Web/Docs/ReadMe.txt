7f84bc492fa6f4f1823d13165e7f659a4204d25f
JessicaSaini <jessicasaini@hotmail.com>
2016-02-18 02:51:18 +0000

    adding context

---
+++ Web/Docs/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿Enable-Migrations -ContextProject WebDataModel -ContextTypeName DIYCMContext -MigrationsDirectory Migrations\DIYCMMigrations
+
+add-migration -ConfigurationTypeName Web.Migrations.DIYCMMigrations.Configuration "InitialCreate"
+
+update-database -ConfigurationTypeName Web.Migrations.DIYCMMigrations.Configuration
---
+++ WebDataModel/DIYCMModels/DIYCMContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDataModel.DIYCMModels
+{
+    class DIYCMContext: DbContext
+    {
+        public DIYCMContext()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<SupplierInvoiceDetail> SupplierInvoiceDetails { get; set; }
+        public DbSet<SupplierInvoiceHeader> SupplierInvoiceHeaders { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<QuoteHeader> QuoteHeaders { get; set; }
+        public DbSet<QuoteDetail> QuoteDetails { get; set; }
+        public DbSet<Document> Documents { get; set; }
+    }
+}
