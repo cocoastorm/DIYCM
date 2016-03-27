3ce352f517cbd06a18162c8de98f03cee3373a2e
Anthony Smith <anthony@smith.lt>
2016-03-26 21:27:00 -0700

    Update page title based on route

--- src/DiyCmWeb/wwwroot/js/diycm.js
+++ src/DiyCmWeb/wwwroot/js/diycm.js
@@ -1,24 +1,33 @@
-﻿var app = angular.module('diycm', ['ngRoute']);
+﻿var app = angular.module('diycm', ['ngRoute', 'LocalStorageModule']);
 
 // setup the routing
 app.config(function ($routeProvider) {
 
     $routeProvider
-      .when('/', {
-          templateUrl: 'views/home.html',
-          controller: 'homeController'
-      });
-
-    $routeProvider
-     .when('/projects', {
-         templateUrl: 'views/projects.html',
-         controller: 'homeController'
-     });
-
-    $routeProvider
+    .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'homeController',
+        title: 'Home'
+    })
+    .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'homeController',
+        title: 'Projects'
+    })
     .when('/singleProject', {
         templateUrl: 'views/singleProject.html',
-        controller: 'homeController'
+        controller: 'homeController',
+        title: 'Project Details'
     });
 
+    $routeProvider.otherwise({ redirectTo: "/home" });
+
+});
+
+// Controls the rootscope
+app.run(function ($rootScope, $route) {
+    $rootScope.$on("$routeChangeSuccess", function (currentRoute, previousRoute) {
+        //Change page title, based on Route information
+        $rootScope.title = $route.current.title;
+    });
 });
