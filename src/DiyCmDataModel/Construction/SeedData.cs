7fd62d9f3bcf6cd2ce1b3618532225cb5e17d650
JessicaSaini <jessicasaini@hotmail.com>
2016-03-31 20:04:13 +0000

    fixing tables

--- src/DiyCmDataModel/Construction/SeedData.cs
+++ src/DiyCmDataModel/Construction/SeedData.cs
@@ -14,12 +14,12 @@ namespace DiyCmDataModel.Construction
                 db.Documents.Add(new Document
                 {
                    DocumentType = "Invoice",
-                    Hyperlink = "http://a.b.c/invoice"
+                   Title = "Invoice March 29th"
                 });
                 db.Documents.Add(new Document
                 {
                     DocumentType = "Quotation",
-                    Hyperlink = "http://a.b.c/quotation"
+                    Title = "Quote March 10th"
                 });
 
                 db.SaveChanges();
@@ -89,86 +89,114 @@ namespace DiyCmDataModel.Construction
                     ProjectId = 100,
                     CategoryId = 100,
                     CategoryName = "Design",
-                    Description = "A category"
+                    Description = "A category",
+                    BudgetAmount = 2000,
+                    ActualAmount = 2400
 
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 110,
-                    CategoryName = "Permits"
+                    CategoryName = "Permits",
+                    BudgetAmount = 2500,
+                    ActualAmount = 2604,
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 120,
-                    CategoryName = "Demolition"
+                    CategoryName = "Demolition",
+                    BudgetAmount = 2200,
+                    ActualAmount = 2240
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 130,
-                    CategoryName = "Excavation"
+                    CategoryName = "Excavation",
+                    BudgetAmount = 10000,
+                    ActualAmount = 6000
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 140,
-                    CategoryName = "Arborist"
+                    CategoryName = "Arborist",
+                    BudgetAmount = 2000,
+                    ActualAmount = 5500
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 150,
-                    CategoryName = "Foundation"
+                    CategoryName = "Foundation",
+                    BudgetAmount = 11000,
+                    ActualAmount = 9000
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 160,
-                    CategoryName = "Framing"
+                    CategoryName = "Framing",
+                    BudgetAmount = 5000,
+                    ActualAmount = 6000
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 170,
-                    CategoryName = "Plumbing"
+                    CategoryName = "Plumbing",
+                    BudgetAmount = 12000,
+                    ActualAmount = 9012
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 180,
-                    CategoryName = "Drywall"
+                    CategoryName = "Drywall",
+                    BudgetAmount = 9000,
+                    ActualAmount = 10133
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 190,
-                    CategoryName = "Electrical"
+                    CategoryName = "Electrical",
+                    BudgetAmount = 13000,
+                    ActualAmount = 11453
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 200,
-                    CategoryName = "Siding"
+                    CategoryName = "Siding",
+                    BudgetAmount = 9600,
+                    ActualAmount = 6002
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 210,
-                    CategoryName = "Painting"
+                    CategoryName = "Painting",
+                    BudgetAmount = 6000,
+                    ActualAmount = 5020
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 220,
-                    CategoryName = "Flooring"
+                    CategoryName = "Flooring",
+                    BudgetAmount = 7000,
+                    ActualAmount = 7560
                 });
                 db.Categories.Add(new Category
                 {
                     ProjectId = 100,
                     CategoryId = 230,
-                    CategoryName = "Security"
+                    CategoryName = "Security",
+                    BudgetAmount = 500,
+                    ActualAmount = 600
                 });
 
                 db.SaveChanges();
@@ -185,54 +213,72 @@ namespace DiyCmDataModel.Construction
                     SubCategoryId = 10,
                     SubCategoryName = "Rough In",
                     CategoryId = 170,
+                    BudgetAmount = 2000,
+                    ActualAmount = 1000
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 20,
                     SubCategoryName = "Faucet",
                     CategoryId = 170,
+                    BudgetAmount = 100,
+                    ActualAmount = 100
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 30,
                     SubCategoryName = "Sink",
                     CategoryId = 170,
+                    BudgetAmount = 200,
+                    ActualAmount = 110
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 40,
                     SubCategoryName = "Bathtub",
                     CategoryId = 170,
+                    BudgetAmount = 600,
+                    ActualAmount = 1000
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 50,
                     SubCategoryName = "Shower",
                     CategoryId = 170,
+                    BudgetAmount = 500,
+                    ActualAmount =450
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 60,
                     SubCategoryName = "Toilet",
                     CategoryId = 170,
+                    BudgetAmount = 200,
+                    ActualAmount = 150
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 70,
                     SubCategoryName = "Bathtub Trim Set",
                     CategoryId = 170,
+                    BudgetAmount = 200,
+                    ActualAmount = 100
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 80,
                     SubCategoryName = "Drain",
                     CategoryId = 170,
+                    BudgetAmount = 200,
+                    ActualAmount = 100
                 });
                 db.SubCategories.Add(new SubCategory
                 {
                     SubCategoryId = 90,
                     SubCategoryName = "Hose Bib",
                     CategoryId = 170,
+                    BudgetAmount = 50,
+                    ActualAmount = 55
                 });
                 db.SaveChanges();
             }
@@ -257,6 +303,7 @@ namespace DiyCmDataModel.Construction
                     AddressPostalCode = "V3L 3M3",
                     ExpiryDate = new DateTime(2016, 1, 31),
                     PercentDiscount = 5,
+                    notes = "A quote for plumbing"
                 });
 
                 db.SaveChanges();
