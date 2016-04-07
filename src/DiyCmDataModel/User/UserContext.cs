555d2e0211096837f64c63cf65088507664d37ca
Ricky Chen <rickyshen1991@hotmail.com>
2016-04-07 00:51:11 +0000

    added user model and user context

---
+++ src/DiyCmDataModel/User/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiyCmDataModel.User
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>().HasKey(m => m.userid);
+        base.OnModelCreating(builder);
+    }
+}
