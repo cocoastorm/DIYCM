1b79256ec0111971467c3bd9bc333852778ce4fc
Jon Deluz <jondeluz@hotmail.com>
2016-04-08 17:12:00 -0700

    Projects now grab their main categories

--- src/DiyCmWeb/wwwroot/js/controllers/singleProjectController.js
+++ src/DiyCmWeb/wwwroot/js/controllers/singleProjectController.js
@@ -2,6 +2,7 @@
     $scope.message = 'Everyone come and look!';
 
     var ProjectId = $routeParams["id"];
+    var baseUrl = 'http://diycm-api.azurewebsites.net/api/';
 
     var onGetProject = function(data) {
         $scope.ActualFinishDate    = (data.ActualFinishDate + "").substring(0, 15);
@@ -9,14 +10,21 @@
         $scope.ProjectedStartDate  = (data.ProjectedStartDate + "").substring(0, 15);
         $scope.ProjectedFinishDate = (data.ProjectedFinishDate + "").substring(0, 15);
         $scope.project = data;
-        console.log(data);
+        //console.log(data);
     };
 
+    var onGetProjectCategories = function (data) {
+        $scope.Categories = data;
+    };
+
+
+
     var onGetProjectError = function(reason) {
-        console.error(reason);
+        //console.error(reason);
     };
 
     ProjectsService.getProject(ProjectId)
         .then(onGetProject, onGetProjectError);
-
+    ProjectsService.getProjectCategories(ProjectId)
+        .then(onGetProjectCategories, onGetProjectError);
 }]);
--- src/DiyCmWeb/wwwroot/js/services/ProjectsService.js
+++ src/DiyCmWeb/wwwroot/js/services/ProjectsService.js
@@ -1,6 +1,7 @@
 (function() {
   var ProjectsService = function ($http, $q) {
     var baseUrl = 'http://diycm-api.azurewebsites.net/api/Projects/';
+    var url = 'http://diycm-api.azurewebsites.net/api/';
     //var baseUrl = 'http://localhost:5000/api/Projects/';
 
 
@@ -42,12 +43,40 @@
           });
     };
 
+    var _ongetProjectCategories = function(id){
+          var reqCategories = $http.get(url + 'categories');
+          var reqProjects = $http.get(url + 'projects/' + id);
+          var arr = new Array();
+          console.log(id);
+          return $q.all([reqCategories, reqProjects]).then(function (values) {
+              var categories = values[0].data;
+              var project = values[1].data;
+              categories.forEach(function (category) {
+              console.log(category.ProjectId)
+                      if (id == category.ProjectId) {
+                        var obj = {
+                          CategoryId      : category.CategoryId,
+                          CategoryName    : category.CategoryName,
+                          Description     : category.CategoryId,
+                          BudgetAmount    : category.BudgetAmount,
+                          ActualAmount    : category.ActualAmount,
+                          PercentCompleted: category.PercentCompleted,
+                          VarianceAmount  : category.VarianceAmount
+                        };
+                        arr.push(obj);
+                      }
+              });
+              return arr;
+          });
+    }
+
     return {
       getProject: _getProject,
       addProject: _addProject,
       editProject: _editProject,
       deleteProject: _deleteProject,
-      getAllProjects: _getAllProjects
+      getAllProjects: _getAllProjects,
+      getProjectCategories: _ongetProjectCategories
     };
   };
 
