0540ffb6779e21a2fe573001da1cd3bb2119d8b3
Jon Deluz <jondeluz@hotmail.com>
2016-04-09 20:27:19 -0700

    Added QuoteHeaders Modal and can now create a quoteheader

--- src/DiyCmWeb/wwwroot/js/controllers/quoteheadersController.js
+++ src/DiyCmWeb/wwwroot/js/controllers/quoteheadersController.js
@@ -11,21 +11,154 @@
       console.log(data);
     };
 
-
-
-
-
-    var onGetAllComplete = function (data) {
-        //console.log(data);
+    var onComplete = function (data) {
+        console.log(data);
+        //window.location.reload();
     };
-    var onGetAllError = function (reason) {
+    var onError = function (reason) {
         console.log(reason);
     };
 
+    $scope.addQuoteHeader = function () {
+      var isAcceptValue;
+      if($scope.quoteheader.IsAccept == true){
+        isAcceptValue = "Y";
+      } else {
+        isAcceptValue = "N";
+      }
+      var data = {
+        Supplier          : $scope.quoteheader.Supplier,
+        Date              : $scope.quoteheader.Date,
+        StartDate         : $scope.quoteheader.StartDate,
+        ExpiryDate        : $scope.quoteheader.ExpiryDate,
+        ReferredBy        : $scope.quoteheader.ReferredBy,
+        AddressStreet     : $scope.quoteheader.AddressStreet,
+        AddressCity       : $scope.quoteheader.AddressCity,
+        AddressProvince   : $scope.quoteheader.AddressProvince,
+        AddressPostalCode : $scope.quoteheader.AddressPostalCode,
+        AddressCountry    : $scope.quoteheader.AddressCountry,
+        PercentDiscount   : $scope.quoteheader.PercentDiscount,
+        notes             : $scope.quoteheader.notes,
+        IsAccept          : isAcceptValue,
+        ContactName       : $scope.quoteheader.ContactName,
+        PhoneNumber       : $scope.quoteheader.PhoneNumber
 
+      };
+      console.log(data);
+      QuotesService.addQuoteHeader(data)
+        .then(onComplete, onError);
+    };
 
     QuotesService.getAllQuoteHeaders()
-    .then(onGetQuoteHeader, onGetAllError);
+    .then(onGetQuoteHeader, onError);
     QuotesService.getAllQuoteHeaders()
-    .then(onGetQuoteHeader, onGetAllError);
+    .then(onGetQuoteHeader, onError);
+
+
+
+
+
+
+          // DatePicker Below
+          $scope.today = function() {
+            $scope.dt = new Date();
+          };
+          $scope.today();
+
+          $scope.clear = function() {
+            $scope.dt = null;
+          };
+
+          $scope.inlineOptions = {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+          };
+
+          $scope.dateOptions = {
+            // dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+          };
+
+          // Disable weekend selection
+          function disabled(data) {
+            var date = data.date,
+              mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+          }
+
+          $scope.toggleMin = function() {
+            $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+            $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+          };
+
+          $scope.toggleMin();
+
+          $scope.open1 = function() {
+            $scope.popup1.opened = true;
+          };
+
+          $scope.open2 = function() {
+            $scope.popup2.opened = true;
+          };
+
+          $scope.open3 = function() {
+            $scope.popup3.opened = true;
+          };
+
+          $scope.setDate = function(year, month, day) {
+            $scope.dt = new Date(year, month, day);
+          };
+
+          $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+          $scope.format = $scope.formats[0];
+          $scope.altInputFormats = ['M!/d!/yyyy'];
+
+          $scope.popup1 = {
+            opened: false
+          };
+
+          $scope.popup2 = {
+            opened: false
+          };
+
+          $scope.popup3 = {
+            opened: false
+          };
+
+          var tomorrow = new Date();
+          tomorrow.setDate(tomorrow.getDate() + 1);
+          var afterTomorrow = new Date();
+          afterTomorrow.setDate(tomorrow.getDate() + 1);
+          $scope.events = [
+            {
+              date: tomorrow,
+              status: 'full'
+            },
+            {
+              date: afterTomorrow,
+              status: 'partially'
+            }
+          ];
+
+          function getDayClass(data) {
+            var date = data.date,
+              mode = data.mode;
+            if (mode === 'day') {
+              var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+              for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                if (dayToCheck === currentDay) {
+                  return $scope.events[i].status;
+                }
+              }
+            }
+
+            return '';
+          }
 }]);
