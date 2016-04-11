a4adc0d95adcedf4621d2b4f5ae77f7a96782fba
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-04-11 14:08:15 -0700

    quick fix to not convert all four digit nums to date objects

--- src/DiyCmWeb/wwwroot/js/interceptors/dateInterceptor.js
+++ src/DiyCmWeb/wwwroot/js/interceptors/dateInterceptor.js
@@ -1,5 +1,5 @@
 (function() {
-  var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+  // var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
   var dateString = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}/;
 
   var convertDateStringsToDates = function(input) {
@@ -13,13 +13,14 @@
         var match;
         // Check for string properties which look like dates.
         // TODO: Improve this regex to better match ISO 8601 date strings.
-        if (typeof value === "string" && (match = value.match(regexIso8601))) {
-            // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
-            var milliseconds = Date.parse(match[0]);
-            if (!isNaN(milliseconds)) {
-                input[key] = new Date(milliseconds);
-            }
-        } else if (typeof value === "string" && (match = value.match(dateString))) {
+        // if (typeof value === "string" && (match = value.match(regexIso8601))) {
+        //     // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+        //     var milliseconds = Date.parse(match[0]);
+        //     if (!isNaN(milliseconds)) {
+        //         input[key] = new Date(milliseconds);
+        //     }
+        // }
+        if (typeof value === "string" && (match = value.match(dateString))) {
             var date = Date.parse(match[0]);
             if (!isNaN(date)) {
                 input[key] = new Date(date);
